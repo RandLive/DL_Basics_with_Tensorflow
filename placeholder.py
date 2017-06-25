5126ea266894363c769077f999a81e99bf901cbf
Mel <dreamflg_dv@hotmail.com>
2017-06-25 09:22:24 +0200

    placeholder

---
+++ placeholder.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 25 09:19:15 2017
+
+@author: ML
+"""
+
+# Solution is available in the other "solution.py" tab
+import tensorflow as tf
+
+
+def run():
+    output = None
+    x = tf.placeholder(tf.int32)
+
+    with tf.Session() as sess:
+        # TODO: Feed the x tensor 123
+        output = sess.run(x, {x:123})
+
+    return output
+
+print(run())
