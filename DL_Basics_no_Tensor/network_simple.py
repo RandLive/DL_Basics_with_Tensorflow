0169c926de12ab190d209bc4988fa2d01a77b022
RandLive <dreamflg_dv@hotmail.com>
2017-07-10 11:24:43 +0200

    DL_Basics

---
+++ DL_Basics_no_Tensor/network_simple.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def sigmoid(x):
+    # TODO: Implement sigmoid function
+    y = 1/(1 + np.exp(-x))
+    return y
+
+inputs = np.array([0.7, -0.3])
+weights = np.array([0.1, 0.8])
+bias = -0.1
+
+# TODO: Calculate the output
+# output = sigmoid(np.sum(inputs*weights)+bias)
+output = sigmoid(np.dot(weights, inputs) + bias)
+
+print('Output:')
+print(output)
